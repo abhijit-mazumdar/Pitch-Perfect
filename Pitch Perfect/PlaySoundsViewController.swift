1320f706ca1d2c8d6d3ffb576277c197d9679dda
Abhijit Mazumdar <pankumazumdar@gmail.com>
2015-04-11 10:56:37 -0700

    Clean up comments

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -42,16 +42,7 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.rate=0.5
         audioPlayer.play()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
     @IBAction func playFastAudio(sender: UIButton) {
         stopAllAudio()
         audioPlayer.currentTime = 0.0
